260155a5c26fb83428c001b5d6517c64e2db5e25
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-04-07 19:13:17 +0300

    fluppet hunter movement strategy

--- src/Bot.scala
+++ src/Bot.scala
@@ -1,18 +1,16 @@
-import util.Random
-
 class ControlFunctionFactory {
   def create = new Bot().respond _
 }
 
 class Bot {
   val movementStrategies = List(
+    MovementStrategy.hunter,
     MovementStrategy.gatherer,
     MovementStrategy.meanderer
   )
 
   def respond(input: String) = {
     val (opcode, params) = CommandParser(input)
-
     opcode match {
       case "React" =>
         val view = View(params("view"))
@@ -23,53 +21,7 @@ class Bot {
   }
 
   private def consultMovementStrategies(strategyIndex: Int = 0, view: View): String = {
-    val response = movementStrategies(strategyIndex).respond(view)
+    val response = movementStrategies(strategyIndex).nextMove(view)
     if (response.isDefined) response.get else consultMovementStrategies(strategyIndex + 1, view)
-  }
-}
-
-abstract class MovementStrategy {
-  def respond(input: View): Option[String]
-}
-
-object MovementStrategy {
-  def meanderer: MovementStrategy = new MovementStrategyMeanderer
-  def gatherer: MovementStrategy = new MovementStrategyGatherer
-
-  private def someMovement(xy: XY) = 
-    Some("Move(dx=%d,dy=%d)".format(xy.x, xy.y))
-
-  private class MovementStrategyGatherer extends MovementStrategy {
-    def respond(view: View): Option[String] = {
-        view.offsetToNearest('P') match {
-        case Some(offset) =>
-          val offsetSignum = offset.signum
-          if (view.cellAtRelPos(offsetSignum) == 'W') None // Don't rush against wall
-          else someMovement(offsetSignum)
-        case None =>
-          None 
-      }
-    }
-  }
-
-  private class MovementStrategyMeanderer extends MovementStrategy {
-    var direction: XY = XY.Right
-
-    def respond(view: View): Option[String] = {
-      val newOffset = nextDirection(view)
-      someMovement(newOffset)
-    }
-
-    private def nextDirection(view: View) = {
-      if (view.cellAtRelPos(direction) == 'W')
-        direction = randomSafeDirection(view)
-
-      direction
-    }
-
-    private def randomSafeDirection(view: View): XY = {
-      val safeCells = view.emptyCellsAround
-      safeCells(new Random().nextInt(safeCells.length))
-    }
   }
 }
---
+++ src/MovementStrategy.scala
@@ -0,0 +1,61 @@
+import util.Random
+
+abstract class MovementStrategy {
+  def nextMove(input: View): Option[String]
+
+  private def someMovement(xy: XY) = 
+    Some("Move(dx=%d,dy=%d)".format(xy.x, xy.y))
+}
+
+object MovementStrategy {
+  def meanderer: MovementStrategy = new MovementStrategyMeanderer
+  def gatherer: MovementStrategy = new MovementStrategyGatherer
+  def hunter: MovementStrategy = new MovementStrategyHunter
+
+  private abstract class AttractedTo extends MovementStrategy {
+    def attraction: Char
+    override def nextMove(view: View): Option[String] = {
+        view.offsetToNearest(attraction) match {
+        case Some(offset) =>
+          val offsetSignum = offset.signum
+          if (view.cellAtRelPos(offsetSignum) == 'W') None // Don't rush against wall
+          else someMovement(offsetSignum)
+        case None =>
+          None 
+      }
+    }
+  }
+
+  private trait FluppetHunter extends AttractedTo {
+    override def attraction = 'B'
+  }
+
+  private trait ZugarGatherer extends AttractedTo {
+    override def attraction = 'P'
+  }
+
+  private class MovementStrategyGatherer extends AttractedTo with ZugarGatherer
+
+  private class MovementStrategyHunter extends AttractedTo with FluppetHunter 
+
+  private class MovementStrategyMeanderer extends MovementStrategy {
+    var direction: XY = XY.Right
+
+    def nextMove(view: View): Option[String] = {
+      val newOffset = nextDirection(view)
+      someMovement(newOffset)
+    }
+
+    private def nextDirection(view: View) = {
+      if (view.cellAtRelPos(direction) == 'W')
+        direction = randomSafeDirection(view) // Start meandering to a new direction
+
+      direction
+    }
+
+    private def randomSafeDirection(view: View): XY = {
+      val safeCells = view.emptyCellsAround
+      safeCells(new Random().nextInt(safeCells.length))
+    }
+  }
+}
